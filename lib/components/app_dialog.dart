63f9f1383dd0c971f446cd2facf369621dd9ef21
Ashish Rai <ashish.rai@appinventiv.com>
2023-10-11 21:53:46 +0530

    Adding Depedencies

---
+++ lib/components/app_dialog.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import '../utils/app_helper/app_color.dart';
+
+class AppDialog extends StatefulWidget {
+  const AppDialog(
+      {super.key, required this.dialogWidget, required this.context});
+  final Widget dialogWidget;
+  final BuildContext context;
+  @override
+  State<AppDialog> createState() => _AppDialogState();
+}
+
+class _AppDialogState extends State<AppDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      decoration: BoxDecoration(
+        color: AppColors.blueSplashScreen,
+        borderRadius: BorderRadius.circular(28),
+        border: Border.all(color: AppColors.white, width: 2),
+      ),
+      child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(child: widget.dialogWidget)
+            ],
+          )),
+    );
+  }
+}
