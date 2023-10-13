c03616a4c2fe39e1af228e3393fd308eec2bc8ae
Ashish Rai <ashish.rai@appinventiv.com>
2023-10-13 18:47:19 +0530

    Improving Bottom Navigation Bar

--- lib/view/home_view/widgets/bottom_navigation.dart
+++ lib/view/home_view/widgets/bottom_navigation.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:whisper/utils/app_helper/app_color.dart';
 
@@ -10,19 +9,77 @@ class AppBottomNavigationBar extends StatefulWidget {
 }
 
 class _AppBottomNavigationBarState extends State<AppBottomNavigationBar> {
+  double? index = 2;
+  double? selectedSize = 40;
+  double? unSelectedSize = 30;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: AppColors.blueSplashScreen
-      ),
-      child: Row(
-        children: [
-          Icon(Icons.search, color: AppColors.white,),
-          Icon(Icons.home_outlined, color: AppColors.white,),
-          Icon(Icons.mark_unread_chat_alt_outlined, color: AppColors.white,),
-          Icon(Icons.settings, color: AppColors.white,),
-        ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: AppColors.blueSplashScreen,
+          borderRadius: BorderRadius.circular(20)
+        ),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 25.0, vertical: 20),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children:
+            [
+              InkWell(
+                onTap: (){
+                  if(index != 0){
+                    index = 0;
+                    setState(() {
+
+                    });
+                  }
+                },
+                child: Icon(Icons.search, color: AppColors.white,size: index == 0? selectedSize : unSelectedSize,)),
+              InkWell(
+                  onTap: (){
+                    if(index != 1){
+                      index = 1;
+                      setState(() {
+
+                      });
+                    }
+                  },
+                  child: Icon(Icons.home_outlined, color: AppColors.white,size: index == 1? selectedSize : unSelectedSize,)),
+              InkWell(
+                  onTap: (){
+                    if(index != 2){
+                      index = 2;
+                      setState(() {
+
+                      });
+                    }
+                  },
+                  child: Icon(Icons.mark_unread_chat_alt_outlined, color: AppColors.white,size: index == 2? selectedSize : unSelectedSize,)),
+              InkWell(
+                  onTap: (){
+                    if(index != 3){
+                      index = 3;
+                      setState(() {
+
+                      });
+                    }
+                  },
+                  child: Icon(Icons.settings, color: AppColors.white,size: index == 3? selectedSize : unSelectedSize,)),
+              InkWell(
+                  onTap: (){
+                    if(index != 4){
+                      index = 4;
+                      setState(() {
+
+                      });
+                    }
+                  },
+                  child: Icon(Icons.menu_open, color: AppColors.white,size: index == 4? selectedSize : unSelectedSize,)),
+            ],
+          ),
+        ),
       ),
     );
   }
